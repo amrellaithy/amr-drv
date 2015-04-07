ce7880cd454f7d44f8161ea798489b296be88bde
amrellaithy <laithy.amr@gmail.com>
2015-04-07 04:39:16 +0200

    Changed topbar, homegrid, products grid. Added individual product page. Improved responsiveness.

--- js/app.js
+++ js/app.js
@@ -3,8 +3,13 @@ $(document).foundation();
 $(document).ready(function(){
   $('.home-slider').slick({
     autoplay: true,
-    fade: true
+    fade: true,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false
   });
+
+
 
   $('.dropdown_button').click(function() {
     $('.dropdown_menu').slideToggle(300, function() {
@@ -16,6 +21,22 @@ $(document).ready(function(){
     horizontalScrolling: false,
     verticalOffset: 40
   });
+
+
+
+
+});
+
+var price = {
+  "0": "EGP 100",
+  "1": "EGP 250",
+  "2": "EGP 500",
+  "3": "EGP 999"
+};
+
+
+$('.size').change(function(){
+  document.getElementById('pprice').innerHTML = "<h3>" + price[$(this).val()] + "</h3>";
 });
 
 
